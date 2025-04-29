1ab3efe317980b7e801b0ce8e338bd40a29f9fb0
oldwestsolutions <oldwestsolutions@users.noreply.github.com>
2025-04-28 22:53:14 -0500

    fix: update Next.js configuration and remove deprecated options

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['images.unsplash.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
-  output: 'standalone',
 };
 
 module.exports = nextConfig; 
